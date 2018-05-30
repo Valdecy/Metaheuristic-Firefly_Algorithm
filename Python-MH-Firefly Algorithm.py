a510fd844b706f09323d619c0bd3cdaf6ef1841a
Valdecy Pereira <valdecypereira@yahoo.com.br>
2018-05-29 23:28:31 -0300

    Update Python-MH-Firefly Algorithm.py

--- Python-MH-Firefly Algorithm.py
+++ Python-MH-Firefly Algorithm.py
@@ -25,8 +25,7 @@ def initial_fireflies(swarm_size = 3, min_values = [-5,-5], max_values = [5,5]):
     for i in range(0, swarm_size):
         for j in range(0, len(min_values)):
              position.iloc[i,j] = random.uniform(min_values[j], max_values[j])
-    for i in range(0, swarm_size):
-         position.iloc[i,-1] = target_function(position.iloc[i,0:position.shape[1]-1])
+        position.iloc[i,-1] = target_function(position.iloc[i,0:position.shape[1]-1])
     return position
 
 # Function: Distance Calculations
@@ -62,20 +61,21 @@ def update_position(position, x, y, alpha_0 = 0.2, beta_0 = 1, gama = 1, firefly
 
 # FA Function
 def firefly_algorithm(swarm_size = 3, min_values = [-5,-5], max_values = [5,5], generations = 50, alpha_0 = 0.2, beta_0 = 1, gama = 1):
-    population = initial_fireflies(swarm_size = swarm_size, min_values = min_values, max_values = max_values)
+    position = initial_fireflies(swarm_size = swarm_size, min_values = min_values, max_values = max_values)
     count = 0
     while (count <= generations):
-        print("Generation: ", count, " of ", generations)
+        print("Generation: ", count, " of ", generations, " f(x) = ", position.iloc[position['Ligth_0'].idxmin(),:][-1])
         for i in range (0, swarm_size):
             for j in range(0, swarm_size):
-                firefly_i = population.iloc[i, 0:population.shape[1]-1].copy(deep = True)
-                firefly_j = population.iloc[j, 0:population.shape[1]-1].copy(deep = True)           
-                ligth_i   = ligth_value(population.iloc[i,-1], firefly_i, firefly_j, gama = gama)
-                ligth_j   = ligth_value(population.iloc[j,-1], firefly_i, firefly_j, gama = gama)
-                if (ligth_i > ligth_j):
-                   population = update_position(population, firefly_i, firefly_j, alpha_0 = alpha_0, beta_0 = beta_0, gama = gama, firefly = i, min_values = min_values, max_values = max_values)
+                if (i != j):
+                    firefly_i = position.iloc[i, 0:position.shape[1]-1].copy(deep = True)
+                    firefly_j = position.iloc[j, 0:position.shape[1]-1].copy(deep = True)           
+                    ligth_i   = ligth_value(position.iloc[i,-1], firefly_i, firefly_j, gama = gama)
+                    ligth_j   = ligth_value(position.iloc[j,-1], firefly_i, firefly_j, gama = gama)
+                    if (ligth_i > ligth_j):
+                       position = update_position(position, firefly_i, firefly_j, alpha_0 = alpha_0, beta_0 = beta_0, gama = gama, firefly = i, min_values = min_values, max_values = max_values)
         count = count + 1
-    best_firefly = population.iloc[population['Ligth_0'].idxmin(),:]
+    best_firefly = position.iloc[position['Ligth_0'].idxmin(),:].copy(deep = True)
     return best_firefly
 
 ######################## Part 1 - Usage ####################################
